32ffab9eb8611db0b1851374cb926d10077e8b68
C457 <android.c357@gmail.com>
2014-11-04 09:29:10 -0600

    Add camera parameter patch

--- gardalte.sh
+++ gardalte.sh
@@ -7,5 +7,8 @@ cd frameworks/av;
 
 # Add Samsung WFD Service -- required for HWComposer
 git fetch https://github.com/cm-gardalte/android_frameworks_av.git cm-11.0;
-git cherry-pick 3400f1f9ad69bfeb4ced169f5a224338cb2ec7d6
+git cherry-pick 3400f1f9ad69bfeb4ced169f5a224338cb2ec7d6;
+
+# camera: Add gardalte camera parameters
+git cherry-pick 8c1d5b13cebf4611a3a23843e49e95db3b3a07f4
 cd ..; cd ..;
