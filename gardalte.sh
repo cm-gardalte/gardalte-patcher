d865c73edba0ee0f5ff7f563a3196fd41d0afc48
C457 <android.c357@gmail.com>
2014-11-10 09:43:07 -0600

    Update camera patch to ps2

--- gardalte.sh
+++ gardalte.sh
@@ -10,5 +10,5 @@ git fetch https://github.com/cm-gardalte/android_frameworks_av.git cm-11.0;
 git cherry-pick 3400f1f9ad69bfeb4ced169f5a224338cb2ec7d6;
 
 # camera: Add gardalte camera parameters
-git cherry-pick 8c1d5b13cebf4611a3a23843e49e95db3b3a07f4
+git cherry-pick 088d48746683765929c91449a6c7f408f439622e
 cd ..; cd ..;
