872debfba020d8bf3963387764dbc0f73a42d00c
C457 <android.c357@gmail.com>
2014-11-15 17:13:06 -0600

    Remove camera patch
    * Utilize below commit
    https://github.com/CyanogenMod/android_frameworks_av/commit/7e69909dd1da9c3602d9e231c85d0efd154951f1

--- gardalte.sh
+++ gardalte.sh
@@ -7,8 +7,5 @@ cd frameworks/av;
 
 # Add Samsung WFD Service -- required for HWComposer
 git fetch https://github.com/cm-gardalte/android_frameworks_av.git cm-11.0;
-git cherry-pick 3400f1f9ad69bfeb4ced169f5a224338cb2ec7d6;
-
-# camera: Add gardalte camera parameters
-git cherry-pick 088d48746683765929c91449a6c7f408f439622e
+git cherry-pick 3400f1f9ad69bfeb4ced169f5a224338cb2ec7d6
 cd ..; cd ..;
