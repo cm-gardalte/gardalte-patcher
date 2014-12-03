fcc8109ea856f5d470d52b8f7b9baead0181c923
c457 <android.c357@gmail.com>
2014-12-03 08:36:10 -0600

    patcher: Update for cm-12.0

--- gardalte.sh
+++ gardalte.sh
@@ -6,6 +6,13 @@
 cd frameworks/av;
 
 # Add Samsung WFD Service -- required for HWComposer
-git fetch https://github.com/cm-gardalte/android_frameworks_av.git cm-11.0;
-git cherry-pick 3400f1f9ad69bfeb4ced169f5a224338cb2ec7d6
+git fetch https://github.com/cm-gardalte/android_frameworks_av.git cm-12.0;
+git cherry-pick d4848efbd5650a48f93a784e5de248a13b830c04
+cd ..;
+
+cd native;
+
+# Revert "Remove Parcel::writeIntPtr."
+git fetch https://github.com/cm-gardalte/android_frameworks_native.git cm-12.0;
+git cherry-pick ce3244f93e8c42ba0f67b449647d2c2e2b5c2f76
 cd ..; cd ..;
