f042b5f0d7088a83267dbad60503d8a58b9f0fbe
c457 <android.c357@gmail.com>
2014-12-26 11:12:33 -0600

    patcher: Update

--- gardalte.sh
+++ gardalte.sh
@@ -2,6 +2,14 @@
 
 # Grab all Samsung Galaxy Light patches
 
+# BUILD
+cd build;
+
+# Recovery: remove wiping of res folder
+git fetch https://github.com/omnirom/android_build.git android-5.0;
+git cherry-pick 12df78d8848325537d64ff0d0370aac6af203866
+cd ..;
+
 # FRAMEWORK
 cd frameworks/av;
 
@@ -15,4 +23,11 @@ cd native;
 # Revert "Remove Parcel::writeIntPtr."
 git fetch https://github.com/cm-gardalte/android_frameworks_native.git cm-12.0;
 git cherry-pick ce3244f93e8c42ba0f67b449647d2c2e2b5c2f76
+cd ..;
+
+cd base;
+
+# Fix volume key music controls and wake up
+git fetch https://github.com/cm-gardalte/android_frameworks_base.git cm-12.0;
+git cherry-pick f92549d029b64a6a822d155cf76179f1e280bdcd
 cd ..; cd ..;
